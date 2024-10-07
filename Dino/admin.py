427c698bac035ad72b3a0244b0a80febc0ed62bb
IvanaMishich <ivana.mishich@rambler.ru>
2024-10-07 18:03:56 +0300

    new model

--- Dino/admin.py
+++ Dino/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Players
+from .models import Players, Levels
+
 
 @admin.register(Players)
 class PlayersAdmin(admin.ModelAdmin):
-    list_display = ('login', 'email', 'score')
+    list_display = ('login', 'email', 'score', 'level',)
     search_fields = ('login',)
     ordering = ('-score',)
+
+
+@admin.register(Levels)
+class LevelsAdmin(admin.ModelAdmin):
+    list_display = ('level', 'score',)
+    ordering = ('level',)
--- Dino/models.py
+++ Dino/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from .managers import Manager
+class Levels(models.Model):
+    level = models.IntegerField(
+        verbose_name="Уровень",
+    )
+    score = models.IntegerField(
+        verbose_name="Очки",
+    )
 class Players(models.Model):
     login = models.CharField(
         max_length=20,
@@ -16,6 +23,13 @@ class Players(models.Model):
         max_length=50,
         verbose_name="Email",
         default='-'
+    )
+    level = models.ForeignKey(
+        Levels,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Уровень",
+        related_name="playerslevel"
     )
     objects = Manager()
 
