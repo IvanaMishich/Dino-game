441cb337b2cf4894380238ab9a3d177b1b8822da
IvanaMishich <ivana.mishich@rambler.ru>
2024-10-10 18:00:48 +0300

    +urls

--- Dino/views.py
+++ Dino/views.py
@@ -3,15 +3,20 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Players, Levels
 from .serializers import PlayersSerializer, LevelsSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PlayersViewSet(viewsets.ModelViewSet):
     queryset = Players.objects.all()
     serializer_class = PlayersSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['login', 'score', 'level']
 
 
 class LevelsViewSet(viewsets.ModelViewSet):
     queryset = Levels.objects.all()
     serializer_class = LevelsSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['level']
 
 # Create your views here.
--- first/urls.py
+++ first/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from Dino.views import PlayersViewSet, LevelsViewSet
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'players', PlayersViewSet)
+router.register(r'levels', LevelsViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
