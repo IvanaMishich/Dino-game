b544a2f33db8b1da1419932adc198cedc1a3dc3e
IvanaMishich <ivana.mishich@rambler.ru>
2024-10-31 16:20:32 +0300

    djavascript code

--- first/settings.py
+++ first/settings.py
@@ -136,3 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 AUTH_USER_MODEL = 'Dino.CustomUser'
+STATICFILES_DIRS = [
+    BASE_DIR / "frontend",
+]
