5b3f301c3c217084e25ce9622201d4ff8af843b6
IvanaMishich <91324294+IvanaMishich@users.noreply.github.com>
2024-11-18 18:03:53 +0300

    Update settings.py

--- first/settings.py
+++ first/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure--h+0zu=31z=jescbjta06_x!ds1*&9gxk%x3fp+j46lz0brm3z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'dino-game-production-e582.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'dino-game-production-01ca.up.railway.app']
 
 
 # Application definition
