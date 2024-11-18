994ea2053ec0aa0c20829bc8659221f75b25d601
IvanaMishich <91324294+IvanaMishich@users.noreply.github.com>
2024-11-18 18:09:51 +0300

    Update settings.py

--- first/settings.py
+++ first/settings.py
@@ -146,4 +146,4 @@ STATICFILES_DIRS = [
 
 LOGIN_URL = '/login/'
 
-CSRF_TRUSTED_ORIGINS = ['https://dino-game-production-e582.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://dino-game-production-01ca.up.railway.app']
