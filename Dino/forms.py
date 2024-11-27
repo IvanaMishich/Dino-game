ce7ffc1f7246de3e56840c5645c24e23683ba568
IvanaMishich <91324294+IvanaMishich@users.noreply.github.com>
2024-11-27 13:24:49 +0300

    Add files via upload

--- Dino/forms.py
+++ Dino/forms.py
@@ -7,10 +7,15 @@ from .models import CustomUser
 # Custom forms that add a hidden 'score' field  /Формы с добавлением скрытого поля 'score'
 class CustomUserCreationForm(UserCreationForm):
     score = forms.IntegerField(widget=forms.HiddenInput(), required=False)
+    consent = forms.BooleanField(
+        required=True,
+        label="I agree to the processing of my personal data",
+        error_messages={'required': 'Your consent to the processing of personal data is required for registration.'}
+    )
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('username',)
+        fields = ('username', 'password1', 'password2', 'consent',)
 
 
 class CustomLoginForm(AuthenticationForm):
